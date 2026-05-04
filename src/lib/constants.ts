3364105509941c621ea0c31c2ae3bedf926b318e
Giovanni Miranda <giovannimiranda09@gmail.com>
2026-05-04 13:24:18 -0400

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const WHATSAPP_LINK = "https://wa.me/556999246403?text=Ol%C3%A1%2C+gostaria+de+saber+mais+sobre+as+solu%C3%A7%C3%B5es+em+Seguran%C3%A7a+do+Trabalho+e+Meio+Ambiente.";
+export const WHATSAPP_LINK = "https://wa.me/5569992464034?text=Ol%C3%A1%2C+gostaria+de+saber+mais+sobre+as+solu%C3%A7%C3%B5es+em+Seguran%C3%A7a+do+Trabalho+e+Meio+Ambiente.";
